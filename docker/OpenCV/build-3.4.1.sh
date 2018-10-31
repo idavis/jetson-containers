658fb56e59eba5620fba239c3c9960919b4dda8b
Ian Davis <ian.f.davis@gmail.com>
2018-10-31 15:44:57 +0000

    The new base image already has an OPENCV_VERSION for the jetpack package, but it only works for Python 2.7. Creating a new variable here for our work.

--- docker/OpenCV/build-3.4.1.sh
+++ docker/OpenCV/build-3.4.1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker build -f Dockerfile -t opencv-install:3.4.1-l4t-28.2.1-tx2-jetpack-3.3-linux-aarch64 --build-arg OPENCV_VERSION=3.4.1 --build-arg L4T_VERSION=28.2.1 --build-arg DEVICE=tx2 --build-arg JETPACK_VERSION=3.3 .
+sudo docker build -f Dockerfile -t opencv-install:3.4.1-l4t-28.2.1-tx2-jetpack-3.3-linux-aarch64 --build-arg OPENCV_GIT_VERSION=3.4.1 --build-arg L4T_VERSION=28.2.1 --build-arg DEVICE=tx2 --build-arg JETPACK_VERSION=3.3 .
