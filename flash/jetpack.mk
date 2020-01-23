a6e3de635a056e92e5f2287c503bde7092ec5155
Ian Davis <ian.f.davis@gmail.com>
2020-01-23 01:31:08 +0000

    Fixing flash config file name mismatch

--- flash/jetpack.mk
+++ flash/jetpack.mk
@@ -9,80 +9,80 @@
 
 
 32.3.1-tx1-jetpack-4.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/tx1 $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.3.1-jax-jetpack-4.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/jax $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.3.1-jax-8gb-jetpack-4.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/jax-8gb $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.3.1-tx2-jetpack-4.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/tx2 $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.3.1-nano-dev-jetpack-4.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/nano-dev $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.3.1-nano-jetpack-4.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/nano $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.3.1-tx2i-jetpack-4.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/tx2i $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.3.1-tx2-4gb-jetpack-4.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/tx2-4gb $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.3.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 
 
 32.2.3-tx1-jetpack-4.2.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/tx1 $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.3-jax-jetpack-4.2.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/jax $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.3-jax-8gb-jetpack-4.2.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/jax-8gb $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.3-tx2-jetpack-4.2.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/tx2 $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.3-nano-dev-jetpack-4.2.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/nano-dev $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.3-nano-jetpack-4.2.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/nano $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.3-tx2i-jetpack-4.2.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/tx2i $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.3-tx2-4gb-jetpack-4.2.3-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/tx2-4gb $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.3" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 
 
 32.2.1-tx1-jetpack-4.2.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/tx1 $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.1-jax-jetpack-4.2.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/jax $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.1-jax-8gb-jetpack-4.2.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/jax-8gb $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.1-tx2-jetpack-4.2.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/tx2 $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.1-nano-dev-jetpack-4.2.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/nano-dev $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.1-nano-jetpack-4.2.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/nano $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.1-tx2i-jetpack-4.2.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/tx2i $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.1-tx2-4gb-jetpack-4.2.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/tx2-4gb $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 
 
 32.2.0-tx1-jetpack-4.2.1-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/tx1 $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.0-jax-jetpack-4.2.1-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/jax $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.0-tx2-jetpack-4.2.1-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/tx2 $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.0-nano-dev-jetpack-4.2.1-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/nano-dev $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.0-nano-jetpack-4.2.1-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/nano $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.0-tx2i-jetpack-4.2.1-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/tx2i $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.2.0-tx2-4gb-jetpack-4.2.1-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/tx2-4gb $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.2.0" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 
 
 32.1-jax-jetpack-4.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.1" $(CURDIR)/build.sh ./conf/jax $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.1-tx2-jetpack-4.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.1" $(CURDIR)/build.sh ./conf/tx2 $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.1-nano-dev-jetpack-4.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.1" $(CURDIR)/build.sh ./conf/nano-dev $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
 32.1-tx2i-jetpack-4.2-image:
-	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.1" $(CURDIR)/build.sh ./conf/tx2i $(REPO):$@)
+	(DOCKER_BUILD_ROOT="$(CURDIR)/l4t/32.1" $(CURDIR)/build.sh ./conf/$@ $(REPO):$@)
